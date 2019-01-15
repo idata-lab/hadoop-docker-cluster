da86503430344ea3c1b3aebc71be0be66155f051
blling <bing.fxx@gmail.com>
2019-01-15 09:32:39 +0800

    Tweak

--- hadoop-docker-cluster.sh
+++ hadoop-docker-cluster.sh
@@ -14,14 +14,14 @@ fi
 print_usage() {
 	echo "Usage: $0 [build|launch|restart|stop|destroy]"
 	echo ""
-	echo "    launch     : create and start hadoop cluster container on docker"
+	echo "    launch   : Create and start hadoop cluster container on docker"
 	echo "    restart  : Restart hadoop cluster container on docker"
 	echo "    stop     : Stop hadoop cluster container on docker"
 	echo "    destroy  : Kill and remove hadoop cluster container on docker, all data lose!!!"
 	echo "    build    : Build docker images with local hadoop binary"
 	echo ""
 	echo "    Options:"
-	echo "        -h,  --help      : Print usage"
+	echo "        -h,  --help  : Print usage"
 	echo "        -s, --slaves : Specify the number of slaves"
 	echo ""
 }
